b894bf1bcf66394bde5c1b85dcd346e9a4858537
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-02-23 14:06:43 -0500

    Create J2.java

---
+++ CCC/2001/J2.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int x = nextInt();
+        int m = nextInt();
+        int n = 1;
+        while (true) {
+            if (n >= m) {
+                System.out.println("No such integer exists.");
+                break;
+            } else if (x * n % m == 1) {
+                System.out.println(n);
+                break;
+            }
+            n++;
+        }
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        return st.nextToken();
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char nextChar() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String nextLine() throws IOException {
+        return br.readLine().trim();
+    }
+}
