cfde3408585c1cec99b1a5b25bdc05fdb0bdee58
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-02-23 13:07:06 -0500

    Create J3.java

---
+++ CCC/2019/J3.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int lines = nextInt();
+        for (int i = 0; i < lines; i++) {
+            String line = next();
+            for (int j = 0; j < line.length();) {
+                int count = 1;
+                for (int k = j; k < line.length() - 1; k++) {
+                    if (line.charAt(k) != line.charAt(k + 1)) {
+                        break;
+                    }
+                    count++;
+                }
+                System.out.print(count + " " + (char) line.charAt(j) + " ");
+                j += count;
+            }
+            System.out.println("");
+        }
+    }
+    
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        return st.nextToken();
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char nextChar() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String nextLine() throws IOException {
+        return br.readLine().trim();
+    }
+}
