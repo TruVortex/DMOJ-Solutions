38f02d9af98d289d4f7ad2428e19bf0502179371
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-02-24 10:41:21 -0500

    Create J2.java

---
+++ CCC/2004/J2.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int n = nextInt();
+        int m = nextInt();
+        for (int i = n; i <= m; i += 60) {
+            System.out.println("All positions change in year " + i);
+        }
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        return st.nextToken();
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char nextChar() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String nextLine() throws IOException {
+        return br.readLine().trim();
+    }
+}
