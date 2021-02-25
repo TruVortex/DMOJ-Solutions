a0d7757d9e607128165590e503e7c0a62bf22ee1
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-02-25 10:43:31 -0500

    Create S1.java

---
+++ CCC/2007/S1.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int n = nextInt();
+        for (int i = 0; i < n; i++) {
+            int y = nextInt();
+            int m = nextInt();
+            int d = nextInt();
+            if (y < 1989 || (y == 1989 && m < 2) || y == 1989 && m == 2 && d <= 27) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        return st.nextToken();
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char nextChar() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String nextLine() throws IOException {
+        return br.readLine().trim();
+    }
+}
