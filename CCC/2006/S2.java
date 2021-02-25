8c13863dc9dccd013a559f82c3a472e861e4993d
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-02-25 10:26:16 -0500

    Create S2.java

---
+++ CCC/2006/S2.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        String str = nextLine();
+        String c1 = nextLine();
+        String c2 = nextLine();
+        for (int i = 0; i < c2.length(); i++) {
+            int idx = c1.indexOf(c2.charAt(i));
+            if (idx == -1) {
+                System.out.print(".");
+            } else {
+                System.out.print(str.charAt(idx));
+            }
+        }
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        return st.nextToken();
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char nextChar() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String nextLine() throws IOException {
+        return br.readLine().trim();
+    }
+}
