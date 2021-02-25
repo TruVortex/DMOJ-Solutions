e82896f4118cdc3994570b8bf66fd8763ac3f055
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-02-25 14:35:22 -0500

    Create S2.java

---
+++ CCC/2008/S2.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int radius = nextInt();
+        while (radius != 0) {
+            int pennies = 0;
+            int temp = radius * radius;
+            for (int i = 1; i <= radius; i++) {
+                pennies += Math.sqrt(temp - (i * i)) + 1;
+            }
+            System.out.println(4 * pennies + 1);
+            radius = nextInt();
+        }
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        return st.nextToken();
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char nextChar() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String nextLine() throws IOException {
+        return br.readLine().trim();
+    }
+}
