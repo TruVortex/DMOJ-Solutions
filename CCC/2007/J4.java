58ee6a6640a9b286d5cbfe4009a3f46691b04b0d
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-02-25 10:39:06 -0500

    Create J4.java

---
+++ CCC/2007/J4.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        char[] arr1 = nextLine().replace(" ", "").toCharArray();
+        char[] arr2 = nextLine().replace(" ", "").toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        if (Arrays.equals(arr1, arr2)) {
+            System.out.println("Is an anagram.");
+        } else {
+            System.out.println("Is not an anagram.");
+        }
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        return st.nextToken();
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char nextChar() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String nextLine() throws IOException {
+        return br.readLine().trim();
+    }
+}
