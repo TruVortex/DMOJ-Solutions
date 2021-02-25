95baf43962f1ef4c1c191ff9207d10463543b116
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-02-25 15:01:44 -0500

    Create S1.java

---
+++ CCC/2009/S1.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int a = nextInt();
+        int b = nextInt();
+        int count = 0;
+        for (int i = 1; i < 22; i++) {
+            int n = (int) Math.pow(i, 6);
+            if (n >= a && n <= b) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        return st.nextToken();
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char nextChar() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String nextLine() throws IOException {
+        return br.readLine().trim();
+    }
+}
