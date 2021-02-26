18b21a12f5f23606a72b32eca4e9ef8df5be2f88
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-02-25 20:20:20 -0500

    Update S4.java

--- CCC/2011/S4.java
+++ CCC/2011/S4.java
@@ -13,9 +13,9 @@ public class Main {
         for (int i = 0; i < 8; i++) {
             arr2[i] = nextInt();
         }
-        System.out.println(get(0, 0) + get(1, 1) + get(0, 1) + get(2, 2) + get(0, 2) + get(4, 4) + get(0, 4) + get(3, 3) + get(2, 3) + get(1, 3) + get(0, 3)
-                + get(5, 5) + get(4, 5) + get(1, 5) + get(0, 5) + get(6, 6) + get(4, 6) + get(2, 6) + get(0, 6) + get(7, 7) + get(6, 7) + get(5, 7) + get(4, 7)
-                + get(3, 7) + get(2, 7) + get(1, 7) + get(0, 7));
+        System.out.println(get(0, 0) + get(1, 1) + get(0, 1) + get(2, 2) + get(0, 2) + get(4, 4) + get(0, 4) + get(3, 3) + get(2, 3) + get(1, 3) + get(0, 3) + get(5, 5)
+                + get(4, 5) + get(1, 5) + get(0, 5) + get(6, 6) + get(4, 6) + get(2, 6) + get(0, 6) + get(7, 7) + get(6, 7) + get(5, 7) + get(4, 7) + get(3, 7) + get(2, 7)
+                + get(1, 7) + get(0, 7));
     }
 
     static int[] arr1 = new int[8];
