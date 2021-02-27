f8a387d6bb3b994df424384658af71ddbacc797c
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-02-27 14:23:30 -0500

    Create S2.java

---
+++ Mock CCC/2020/S2.java
@@ -0,0 +1,77 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int n = nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = nextInt();
+            }
+        }
+        int ans = 0;
+        ArrayList<String> list = new ArrayList();
+        for (int i = 0; i < n; i++) {
+            if (arr[0][i] == 1) {
+                ans++;
+                list.add("C " + (i + 1));
+                for (int j = 0; j < n; j++) {
+                    arr[j][i] ^= 1;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i][0] == 1) {
+                ans++;
+                list.add("R " + (i + 1));
+                for (int j = 0; j < n; j++) {
+                    arr[i][j] ^= 1;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[i][j] == 1) {
+                    System.out.println(-1);
+                    return;
+                }
+            }
+        }
+        System.out.println(ans);
+        for (int i = 0; i < ans; i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        return st.nextToken();
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char nextChar() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String nextLine() throws IOException {
+        return br.readLine().trim();
+    }
+}
