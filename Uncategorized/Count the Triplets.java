69850de75e741e759fb13d10a8e6bf3b25a42047
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-03-08 19:07:26 -0500

    Create Count the Triplets.java

---
+++ Uncategorized/Count the Triplets.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int n = nextInt();
+        int[] arr = new int[n];
+        Set<Integer> set = new HashSet();
+        for (int i = 0; i < n; i++) {
+            arr[i] = nextInt();
+            set.add(arr[i]);
+        }
+        Arrays.sort(arr);
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (i != 0 && arr[i] == arr[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < n; j++) {
+                if (j > i + 1 && arr[j] == arr[j - 1]) {
+                    continue;
+                }
+                if (set.contains(arr[i] + arr[j])) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        return st.nextToken();
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char nextChar() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String nextLine() throws IOException {
+        return br.readLine().trim();
+    }
+}
