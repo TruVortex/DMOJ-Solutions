9e776358582a0c7011472f6b508e456d4a38d8f7
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-03-30 15:36:10 -0400

    Create P2.java

---
+++ VM7WC/2015/C1/P2.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int n = nextInt();
+        for (int i = 0; i < n; i++) {
+            int l = nextInt();
+            int[] arr = new int[l];
+            for (int j = 0; j < l; j++) {
+                arr[j] = nextInt();
+            }
+            System.out.println(insertionSort(arr));
+        }
+    }
+    
+    static int insertionSort(int[] arr) {
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int j = i;
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+                j--;
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        return st.nextToken();
+    }
+
+    static long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char nextChar() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String nextLine() throws IOException {
+        return br.readLine().trim();
+    }
+}
