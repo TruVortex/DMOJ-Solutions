376470c97b5f3f8b5330fc7e4dde46840164c761
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-05-03 11:13:07 -0400

    Create P2.cpp

---
+++ CPC/2019/C1/P2.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <algorithm>
+#define scan(x) while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0')
+char _;
+
+using namespace std;
+
+int main() {
+    int n, k, l = 0, r = 1, ans = 0;
+    scan(n);
+    scan(k);
+    int luggage[n];
+    for (int i = 0; i < n; i++) {
+        scan(luggage[i]);
+    }
+    sort(luggage, luggage + n);
+    while (l < n && r < n) {
+        if (luggage[r] - luggage[l] <= k) {
+			r++;
+		} else if (luggage[r] - luggage[l] > k) {
+			l++;
+        }
+		ans = max(ans, r - l);
+    }
+    printf("%d", ans);
+    return 0;
+}
