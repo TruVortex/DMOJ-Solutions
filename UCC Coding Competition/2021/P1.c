3d79f05d3fb8881719a7c99956f24f1bbea89981
TruVortex <75190116+TruVortex@users.noreply.github.com>
2021-05-24 10:40:34 -0400

    Create P1.c

---
+++ UCC Coding Competition/2021/P1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int ans = 0;
+    char ch = getchar();
+    while (ch != '\n') {
+        if (ch == '2') {
+            ans++;
+        } else if (ch == '5') {
+            ans--;
+        }
+        ch = getchar();
+    }
+    printf("%d", ans);
+}
